7013d7d1e92ced8bdd6d7221cf2af3f7e749569b
pmeena <pankaj.jpm.007@gmail.com>
2025-01-14 14:57:31 +0100

    type command

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,33 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  const int nEchoLength = 5;
+  const int nTypeLength = 5;
 
-  while (std::cin)
+  std::vector<std::string> sBuiltIn = { "exit", "echo", "type" };
+
+  std::string input;
+  while (std::getline(std::cin, input))
   {
-	std::cout << "$ ";
-	std::string input;
-	std::getline(std::cin, input, '\n');
-	if (input == "exit 0")
+	std::cout << "$ "; // prompt
+
+	if (input.find("exit") == 0) // exit command
 		return 0;
-	if (input.find("echo ") != std::string::npos) // if you find "echo " in the input stream
+
+	if (input.find("echo") == 0) // echo command
 	{
-		std::cout << input.substr(input.find("echo ") + 5) << '\n'; // print everything after "echo "
+		std::cout << input.substr(nEchoLength) << '\n'; // print everything after "echo "
 		continue;
 	}
-	std::cerr << input << ": command not found\n";
+
+	if (input.find("type") == 0) // type command
+	{
+		std::string sSub = input.substr(nTypeLength); // get the command following "type"
+		if (find(sBuiltIn.begin(), sBuiltIn.end(), sSub) != sBuiltIn.end()) // check if the command exists in built-in commands
+			cout << sSub << " is a shell builtin\n";
+		continue;
+	}
+
+	std::cout << input << ": command not found\n";	// invalid command
   }
 }
