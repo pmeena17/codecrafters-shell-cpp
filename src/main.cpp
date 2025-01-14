861578cc1286608dc7e755f01242d4b42a1e6467
pmeena <pankaj.jpm.007@gmail.com>
2025-01-14 15:54:06 +0100

    restructuring

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,49 @@
 
 int main()
 {
-    // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
     const int nEchoLength = 5;
     const int nTypeLength = 5;
+    static bool bExit = false;
 
-    std::vector<std::string> sBuiltIn = { "exit", "echo", "type" };
-
-    std::string input;
-    while (std::getline(std::cin, input))
+    while (!bExit)
     {
+        // Flush after every std::cout / std:cerr
+        std::cout << std::unitbuf;
+        std::cerr << std::unitbuf;
+
+
+        std::string input;
+        std::getline(std::cin, input);
+        bool bIsInputValid = false;
         std::cout << "$ "; // prompt
 
         if (input.find("exit") == 0) // exit command
-            return 0;
+        {
+            bExit = true;
+            bIsInputValid = true;
+            continue;
+        }
 
         if (input.find("echo") == 0) // echo command
         {
             std::cout << input.substr(nEchoLength) << '\n'; // print everything after "echo "
+            bIsInputValid = true;
             continue;
         }
 
         if (input.find("type") == 0) // type command
         {
             std::string sSub = input.substr(nTypeLength); // get the command following "type"
-            if (sSub == "exit" || sSub == "echo" || sSub ==  "type") // check if the command matches any built-in commands. TODO: use sBuiltIn for look-up?
+            if (sSub.substr(0, 4) == "exit" || sSub.substr(0, 4) == "echo" || sSub.substr(0, 4) == "type") // check if the command matches any built-in commands. TODO: use sBuiltIn for look-up?
                 std::cout << sSub << " is a shell builtin\n";
+            else
+                std::cout << sSub << ": not found\n";
+            bIsInputValid = true;
             continue;
         }
 
-        std::cout << input << ": command not found\n";	// invalid command
-        std::cout << "$ "; // prompt
+        if (!bIsInputValid)
+            std::cout << input << ": command not found\n";	// invalid command
     }
+    return 0;
 }
